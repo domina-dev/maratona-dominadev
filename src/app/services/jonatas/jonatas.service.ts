c0766bd4c0b918f38c5064a46f7845bee455b2d4
Jonatas <129806754+jonatasnahorny@users.noreply.github.com>
2023-11-28 12:24:18 -0300

    Dia 1: Funções mostrarNome e mostrarMedia

--- src/app/services/jonatas/jonatas.service.ts
+++ src/app/services/jonatas/jonatas.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JonatasService {
+  // SEMANA 1
+  mostrarNome() {
+    let nome: string = 'Jonatas';
+    this.auxiliarNome(nome);
+  }
 
-  constructor() { }
-
-  teste(){
-    alert("Teste Jonatas Executado com sucesso!")
+  private auxiliarNome(nome: string) {
+    console.log('O nome é', nome);
+  }
+  mostrarMedia() {
+    let media: number = (1 + 3 + 5 + 10) / 3;
+    console.log('A media dos valores é', media);
   }
 }
--- src/app/services/jonatas/tarefas-jonatas.ts
+++ src/app/services/jonatas/tarefas-jonatas.ts
@@ -1,7 +1,7 @@
+import { Compilacao } from 'src/app/model/compilacao';
+import { Alunos, Status } from 'src/app/pages/painel-compilacao/data';
 
-import { Compilacao } from "src/app/model/compilacao";
-import { Alunos, Status } from "src/app/pages/painel-compilacao/data";
-
-export const TarefasJonatas: Compilacao[]  = [
-    { autor: Alunos.JONATAS, status: Status.OK, funcao: "teste" },
-]
+export const TarefasJonatas: Compilacao[] = [
+  { autor: Alunos.JONATAS, status: Status.OK, funcao: 'mostrarNome' },
+  { autor: Alunos.JONATAS, status: Status.OK, funcao: 'mostrarMedia' }
+];
