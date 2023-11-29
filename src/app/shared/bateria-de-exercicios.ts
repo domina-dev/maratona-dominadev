77bc30dfeff49490ea25d7b708988f2e120fe50a
Vitor Macedo <evitorfiler@gmail.com>
2023-11-29 20:14:17 -0300

    incremento, bateria de exercícios

---
+++ src/app/shared/bateria-de-exercicios.ts
@@ -0,0 +1,30 @@
+/**
+ * Crie Funções para: 
+ * 
+===============================================================================================================================================
+    1 - Retornar a soma de dois números recebidos
+===============================================================================================================================================
+    2 - Receber uma hora qualquer do dia em formato 24h e informar se é manhã, dia, tarde, etc...
+===============================================================================================================================================
+    3 - Receber um 'objeto de cliente (1)' e incluir os atributos que faltam, são eles: Nacionalidade, Endereço, id, data de nascimento
+        Pode utilizar dados aleatórios, mas o objeto final deve ser retornado pelo menos em um log com todos os atributos prenchidos
+        (1) - {
+                nome: "Vítor Nunes Macêdo",
+                idade: 24,
+                naturalidade: "Itapira - SP",
+
+            }
+===============================================================================================================================================
+    4 - Percorrer a lista de produtos que se encontra neste arquivo e informar qual o produto mais caro e o mais barato
+===============================================================================================================================================
+	5 - fazer o exercício 4 utilizando o .find() do TS
+===============================================================================================================================================
+*/
+
+export const listaProdutos: any = [
+	{ nome: "Metralhadora Ak-45 Pente alongado", valor: 20000 },
+	{ nome: "Iphone 13", valor: 4300 },
+	{ nome: "Carrinho de rolimã ", valor: 200 },
+	{ nome: "Sabugo de milho semi novo Único dono", valor: 1 },
+	{ nome: "Retrovisor Marea 98", valor: 86 },
+]
--- src/app/shared/semana1/dia1.ts
+++ src/app/shared/semana1/dia1.ts
@@ -2,11 +2,14 @@
     Funções
     Variáveis - let
     Tipos Primitivos - string - number - boolean - any - null/undefined
+
+===============================================================================================================================================
+    1 - Faça uma função que apresente um log com o nome informado
+===============================================================================================================================================
+    2 - Função que recebe 3 números e calcula a média entre eles
+===============================================================================================================================================
 */
 
-//Faça uma função que apresente um log com o nome informado
-
-//Função que recebe 3 números e calcula a média entre eles
 
 
 
