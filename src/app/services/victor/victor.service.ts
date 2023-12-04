64292be0fb61ce0278a6726249c6201a7eef3dc1
Victor <vitorsntossouza9866@gmail.com>
2023-12-04 20:37:29 -0300

    finalizando atividade 5 dia 2

--- src/app/services/victor/victor.service.ts
+++ src/app/services/victor/victor.service.ts
@@ -21,6 +21,7 @@ export class VictorService extends BaseService {
     console.log(nome);
   };
 
+
   //-----------------EXERCIO 2 DIA 1-----------------
   /**
    *
@@ -33,6 +34,7 @@ export class VictorService extends BaseService {
     let media: number = soma / 3;
     console.log(media)
   };
+
 
   //-----------------EXERCIO 1 DIA 2-----------------
   /**
@@ -75,26 +77,26 @@ export class VictorService extends BaseService {
       alert('Essa pessoa e MAIOR de idade')
     };
 
-    let date = new Date(anoNasc, mesNasc-1, diaNasc);
+    let date = new Date(anoNasc, mesNasc - 1, diaNasc);
     let diaAnoNasc = this.diaDoAno(date);
     let diaHoje = this.diaDoAno(new Date());
     let restante = diaHoje - diaAnoNasc;
 
-    if(restante < 0){
-      alert("Faltam "+ (Math.abs(restante)-1)+ "dias para o aniversario");
-    }else{
+    if (restante < 0) {
+      alert("Faltam " + (Math.abs(restante) - 1) + "dias para o aniversario");
+    } else {
       let dias = Math.abs(restante - 365);
-      alert(`O último aniversario foi há ${Math.abs(restante)+1} dias
+      alert(`O último aniversario foi há ${Math.abs(restante) + 1} dias
       \nFaltam ${dias} dias para o proximo aniversario`);
     }
   }
 
-  private diaDoAno(data: any){
+  private diaDoAno(data: any) {
     let inicioAno: any = new Date(data.getFullYear(), 0, 0);
     let diferenca: any = data - inicioAno;
-    let umDiaEmMilisegundos = 1000 * 60 *60 *24;
+    let umDiaEmMilisegundos = 1000 * 60 * 60 * 24;
     let diaAno = Math.floor(diferenca / umDiaEmMilisegundos);
-    console.log('Dia do ano é: '+ diaAno);
+    console.log('Dia do ano é: ' + diaAno);
     return diaAno;
   }
 
@@ -106,43 +108,87 @@ export class VictorService extends BaseService {
    */
   pessoaEleitoral(idade?: any) {
     if (idade <= 15) {
-      alert("Essa pessoa NÃO VOTA, pois tem "+ idade +" anos")
-    }else if (idade == 16 || idade == 17 || idade > 70) {
-      alert("Essa pessoa tem o voto OPICIONAL, pois tem "+ idade + " anos")
-    }else {
-      alert("Essa pessoa tem o voto OBRIGATORIO, pois ela tem "+ idade +" anos")
+      alert("Essa pessoa NÃO VOTA, pois tem " + idade + " anos")
+    } else if (idade == 16 || idade == 17 || idade > 70) {
+      alert("Essa pessoa tem o voto OPICIONAL, pois tem " + idade + " anos")
+    } else {
+      alert("Essa pessoa tem o voto OBRIGATORIO, pois ela tem " + idade + " anos")
     }
   }
 
 
+  //-----------------EXERCIO 4 DIA 2-----------------
   /**
    *
    * @param valor number
    * @param moeda string
    */
-  //-----------------EXERCIO 4 DIA 2-----------------
   valorPagamento(valor?: any, moeda?: any) {
 
-    let conversao: number = 1 ;
+    let conversao: number = 1;
 
-    if (moeda == 'dolar') {
+    if (moeda.toLowerCase() == 'dolar') {
       conversao = valor * 4.93
-      console.log(`Voce recebeu ${valor} Dólar/USD de pagamento, que equivale a ${conversao} Reais/BRL`)
+      console.log(`Voce recebeu ${valor} Dólar/USD de pagamento, que equivale a ${conversao} Reais/BRL, valor atual do dolar é US$4.93`)
     }
-    else if (moeda == 'libras') {
+    else if (moeda.toLowerCase() == 'libras') {
       conversao = valor * 6.24
-      console.log(`Voce recebeu ${valor} Libra/GBP de pagamento, que equivale a ${conversao} Reais/BRL`)
+      console.log(`Voce recebeu ${valor} Libra/GBP de pagamento, que equivale a ${conversao} Reais/BRL, valor atual da Libra é £6.24`)
     }
-    else if(moeda == 'euro') {
+    else if (moeda.toLowerCase() == 'euro') {
       conversao = valor * 5.36
-      console.log(`Voce recebeu ${valor} Euro/EUR de pagamento, que equivale a ${conversao} Reais/BRL`)
+      console.log(`Voce recebeu ${valor} Euro/EUR de pagamento, que equivale a ${conversao} Reais/BRL, valor atual do Euro é €5.36`)
     }
-    else if(moeda == 'iene') {
+    else if (moeda.toLowerCase() == 'iene') {
       conversao = valor * 0.034
-      console.log(`Voce recebeu ${valor} Iene/JPY de pagamento, que equivale a ${conversao} Reais/BRL`)
+      console.log(`Voce recebeu ${valor} Iene/JPY de pagamento, que equivale a ${conversao} Reais/BR, valor atual do Iene é ¥0.034`)
     }
-    else if(moeda == 'real'){
+    else if (moeda.toLowerCase() == 'real') {
       console.log(`Voce recebeu ${valor} Real/BRL de pagamento `)
+    }
+    else{
+      alert('ATENÇÃO! Parece que voce não recebeu valor nenhum')
+    }
+  }
+
+
+  //-----------------EXERCIO 5 DIA 2-----------------   , moeda?: any
+  /**
+   *
+   * @param valor number
+   * @param moeda string
+   */
+  valorRecebido(valor?: any) {
+    let conversaoMoeda: number = 1;
+    let moeda: string = 'Dolar'
+    switch (moeda.toLowerCase()) {
+      case 'dolar':
+        conversaoMoeda = valor * 4.93;
+        alert(`Voce acabou de receber um valor de ${valor} Dólar/USD, convertendo para o Real/BRL corresponde a ${conversaoMoeda} Reais/BRL, valor atual do dolar é US$4.93`)
+        break;
+
+      case 'libras':
+        conversaoMoeda = valor * 6.24;
+        alert(`Voce acabou de receber um valor de ${valor} Libra/GBP, convertendo para o Real/BRL corresponde a ${conversaoMoeda} Reais/BRL, valor atual da Libra é £6.24`)
+        break;
+
+      case 'euro':
+        conversaoMoeda = valor * 5.36;
+        alert(`Voce acabou de receber um valor de ${valor} Euro/EUR, convertendo para o Real/BRL corresponde a ${conversaoMoeda} Reais/BRL, valor atual do Euro é €5.36`)
+        break;
+
+      case 'iene':
+        conversaoMoeda = valor * 0.034;
+        alert(`Voce acabou de receber um valor de ${valor} Iene/JPY, convertendo para o Real/BRL corresponde a ${conversaoMoeda} Reais/BRL, valor atual do Iene é ¥0.034`)
+        break;
+
+      case 'real':
+        alert(`Voce acabou de receber um valor de ${valor} Real/BRL`)
+        break;
+
+      default:
+        alert('ATENÇÃO! Parece que voce não recebeu valor nenhum')
+        break;
     }
   }
 }
