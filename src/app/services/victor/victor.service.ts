53ea1582d846538082e0d04fb18b4e0a3e1ef425
Vitor Macedo <evitorfiler@gmail.com>
2023-12-04 20:36:12 -0300

    ajustes aula

--- src/app/services/victor/victor.service.ts
+++ src/app/services/victor/victor.service.ts
@@ -34,4 +34,35 @@ export class VictorService extends BaseService {
     let media: number = soma / 3;
     console.log(media)
   };
+
+  /**
+   * 
+   * @param diaNasc number
+   * @param mesNasc number
+   * @param anoNasc number
+   */
+  calculaAniversario(diaNasc?: any, mesNasc?: any, anoNasc?: any) {
+    let date = new Date(anoNasc, mesNasc-1, diaNasc)
+    let diaDoAnoNasc = this.diaDoAno(date);
+    let diaDeHoje = this.diaDoAno(new Date());
+
+    let restante = diaDeHoje - diaDoAnoNasc;
+    if(restante < 0){
+      alert("Faltam " + (Math.abs(restante)-1) + " dias para o aniversário");
+    }else{
+      let dias = Math.abs(restante - 365);
+      alert(`o aniversário foi há ${Math.abs(restante) + 1} dias
+        \nFaltam ${dias} dias para o próximo aniversário`);
+    }
+
+  }
+
+  private diaDoAno(data: any) {
+    let inicioDoANo: any = new Date(data.getFullYear(), 0, 0);
+    let diferenca: any = data - inicioDoANo;
+    let umDiaEmMilissegundos = 1000 * 60 * 60 * 24;
+    let diaDoAno = Math.floor(diferenca / umDiaEmMilissegundos);
+    console.log('Day of year: ' + diaDoAno);
+    return diaDoAno;
+  }
 }
