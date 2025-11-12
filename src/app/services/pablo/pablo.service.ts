fa2f70d59163fc24a216d52898cf72d452e0f038
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2025-11-11 22:12:08 -0300

    #feature/pablo - teste do dia1

--- src/app/services/pablo/pablo.service.ts
+++ src/app/services/pablo/pablo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from '../base.service';
+import { log } from 'util';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,12 @@ export class PabloService extends BaseService {
     super();
   }
 
+  mostrarNome(nome:string){
+    console.log("Nome recebido:",nome)
+  }
+
+  calcularNumero(numero1:number ,numero2:number, numero3:number){
+    let resultado =  (numero1 + numero2 + numero3)/ 3;
+    console.log(resultado);
+}
 }
