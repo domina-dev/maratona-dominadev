ca4e944a99b54e029256e2d1ba6f099508caa47d
PABLO ALVES OLIVEIRA <pabloalves1213@gmail.com>
2025-11-18 20:59:52 -0300

    #featture/pablo - subindo task de estudos

--- src/app/services/pablo/pablo.service.ts
+++ src/app/services/pablo/pablo.service.ts
@@ -11,12 +11,57 @@ export class PabloService extends BaseService {
     super();
   }
 
-  mostrarNome(nome:string){
-    console.log("Nome recebido:",nome)
+  mostrarNome(nome: string) {
+    console.log("Nome recebido:", nome)
   }
 
-  calcularNumero(numero1:number ,numero2:number, numero3:number){
-    let resultado =  (numero1 + numero2 + numero3)/ 3;
+  calcularNumero(numero1: number, numero2: number, numero3: number) {
+    let resultado = (numero1 + numero2 + numero3) / 3;
     console.log(resultado);
+  }
+
+  mostraIdade(dataNascimento: Date) {
+    let hoje = new Date();
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear()
+    if (idade >= 18) {
+      console.log("maior de idade", idade)
+    }
+    else {
+      console.log("menor de idade", idade);
+    }
+  }
+
+  verificacaoEleitoral(dataNascimento: Date) {
+    let hoje = new Date();
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear()
+    if (idade <= 15) {
+      alert("15 anos ou menos não vota")
+    }
+    else if (idade == 16 || idade == 17) {
+      alert("16 e 17 anos voto opcional")
+    }
+    else if (idade >= 18 && idade <= 70) {
+      alert("Entre 18 e 70 é obrigatório")
+    }
+    else if (idade >= 70) {
+      alert("Acima de 70 é opcional")
+    }
+  }
+  /**
+   * 
+   * @param numero 
+   * 
+   */       
+  analisarLista(numero: number) {
+    let lista = [0, 2, 3, 8, 17]
+    const numeroLista = lista.length;
+    if (lista.length === 0) {
+      console.log("lista vazia");
+
+    }
+    console.log(numeroLista);
+    { numeroLista };
+  }
 }
-}
+
+
